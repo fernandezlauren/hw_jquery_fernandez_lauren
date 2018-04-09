268d7c51a812ebff6483366676ecf532d7f99f37
Lauren Fernandez <lfernan60@ufl.edu>
2018-04-08 20:50:42 -0400

    Add jquery to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
 $( document ).ready(function() {
-    $('h2').last().text("I'm Lauren Fernandez, and I'm learning jQuery!");
+    	var headings = document.querySelectorAll("h2");
+	headings[headings.length-1].innerText = "I'm Lauren Fernandez, and I'm learning JQuery!";
 });
