329712d0a0aa02a26181ba28ab7f3019afcfa09f
Lauren Fernandez <lfernan60@ufl.edu>
2018-04-08 20:49:25 -0400

    Add jquery to main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+	var paragraphs = document.querySelectorAll("p");
+	$(paragraphs).css({
+    		"background-color": "black",
+       		"font-size": "150%",
+        	"color": "white"
+        });
 });
